d7703655180492e08a5940690558a97fd3d44dfe
Matt Bowcock <matt.bowcock@gmail.com>
2012-05-02 23:30:55 -0400

    Added some test logic to index route. Needs to be expanded on.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/:id?', routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  /*res.render('index', { title: 'Express' })*/
+  var id = req.params.id;
+
+  /* if id passed then parse from base36 */
+  if(id){
+	var url = parseInt(id, 36);
+	res.send(id + " " + url);
+  } else {
+	  res.send("No url sent");
+  }
 };
